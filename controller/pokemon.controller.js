a9ac7d9d6d49cf8c3fb131564c3e4635fc177f0f
IAyrtonTWK <ignacio.aravena2020@twk.cl>
2022-12-06 16:22:27 -0300

    Errores controlados, API Lista

--- controller/pokemon.controller.js
+++ controller/pokemon.controller.js
@@ -35,13 +35,20 @@ const createPokemon = (req, res) => {
 const getPokemons = async (req, res) => {
   Pokemon.find({}, "name heightInMeter weightInKG img url")
     .then((pokemons) => {
-      res.json({
-        ok: true,
-        pokemons,
-      });
+      if(pokemons.length > 0){
+        res.status(200).json({
+          ok: true,
+          pokemons,
+        });
+      } else {
+        res.status(204).json({
+          ok: false,
+          msg: "DB Empty",
+        });
+      }
     })
     .catch(() => {
-      res.json({
+      res.status(500).json({
         ok: false,
         msg: "Get Pokemons failed",
       });
@@ -55,78 +62,21 @@ const getSinglePokemon = (req, res) => {
 
   Pokemon.findById({_id: id})
     .then((pokemon) => {
-      res.json({
+      res.status(200).json({
         ok: true,
-        pokemon,
+        pokemon
       });
     })
     .catch(() => {
-      res.json({
+      res.status(400).json({
         ok: false,
-        msg: `Get Pokemon ID: ${id} failed.`
+        msg: "Get ID Pokemon failed"
       });
     });
 };
-
-// const updatePokemon = (req, res) => {
-//   const { id } = req.params;
-//   const { name, heightInMeter, weightInKG } = req.body;
-
-//   console.log(req.body);
-
-//   const idToBeUpdated = id;
-//   const newName = name;
-//   const newHeightInMeter = heightInMeter;
-//   const newWeightInKG = weightInKG;
-
-//   const pokemonToBeUpdated = Pokemon.find(
-//     (pokemon) => pokemon._id === idToBeUpdated
-//   );
-
-//   if (newName) {
-//     pokemonToBeUpdated.name = newName;
-
-//     res.send(`Pokemon with the id ${idToBeUpdated} has been updated.\n
-//     it now has a updated name ${pokemonToBeUpdated.name}`);
-//   }
-
-//   if (newHeightInMeter) {
-//     pokemonToBeUpdated.heightInMeter = newHeightInMeter;
-
-//     res.send(`Pokemon with the id ${idToBeUpdated} has been updated.\n
-//     it now has a updated last name ${pokemonToBeUpdated.heightInMeter}`);
-//   }
-
-//   if (newWeightInKG) {
-//     pokemonToBeUpdated.weightInKG = newWeightInKG;
-
-//     res.send(`Pokemon with the id ${idToBeUpdated} has been updated.\n
-//     it now has a updated age ${pokemonToBeUpdated.weightInKG}`);
-//   }
-// };
-
-// const deletePokemon = (req, res) => {
-//   const { id } = req.params;
-//   console.log(id);
-//   const idToDelete = id;
-
-//   const pokemonTobeDelete = pokemons.find(
-//     (pokemon) => pokemon.id === idToDelete
-//   );
-//   pokemons = pokemons.filter((pokemon) => pokemon.id !== idToDelete);
-
-//   res.send(`pokemon with
-//   \n id: ${idToDelete}
-//   \n name: ${pokemonTobeDelete.name}
-//   \n heightInMeter: ${pokemonTobeDelete.heightInMeter}
-//   \n weightInKG: ${pokemonTobeDelete.weightInKG}
-//   \n has been deleted from the database.`);
-// };
 
 module.exports = {
   createPokemon,
   getPokemons,
   getSinglePokemon,
-  // updatePokemon,
-  // deletePokemon,
 };
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.json({ message: "API Funcionando" });
+  res.status(404).json({ message: "Not Found " });
 });
 
 app.use("/pokemon", pokemons);
